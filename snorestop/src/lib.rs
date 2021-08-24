b1752bcbff5dc7cd251f71fa38f90591a7d55a8b
Sanae <atmatm6000@outlook.com>
2021-08-24 03:41:22 -0600

    Add temporary __print global

--- snorestop/src/lib.rs
+++ snorestop/src/lib.rs
@@ -1,17 +1,44 @@
 use std::ffi::CStr;
+use std::mem::transmute;
 use std::os::raw::c_char;
 
-use winapi::shared::minwindef::{HMODULE, FARPROC};
-use winapi::um::consoleapi::AllocConsole;
 use detour::static_detour;
-use std::mem::transmute;
-use nodejs::neon::{types::JsString, reflect::eval};
+use nodejs::{
+    neon::{
+        context::{
+            Context,
+            FunctionContext
+        },
+        object::Object,
+        reflect::eval,
+        types::{
+            JsFunction,
+            JsString,
+            JsUndefined
+        },
+        handle::Handle,
+        result::JsResult
+    },
+};
+use winapi::shared::minwindef::{FARPROC, HMODULE};
+use winapi::um::consoleapi::AllocConsole;
 
 static_detour! {
   static Il2cppInitDetour: unsafe extern "C" fn(*const c_char) -> bool;
 }
 
 type Il2cppInit = unsafe extern "C" fn(*const c_char) -> bool;
+
+fn __print(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    if cx.len() < 1 {
+        println!();
+    } else {
+        let arg: Handle<JsString> = cx.argument(0).unwrap();
+        let text: Handle<JsString> = arg.downcast_or_throw(&mut cx).unwrap();
+        println!("{}", text.value(&mut cx))
+    }
+    Ok(JsUndefined::new(&mut cx))
+}
 
 fn il2cpp_init(domain_name: *const c_char) -> bool {
     unsafe {
@@ -27,6 +54,9 @@ fn il2cpp_init(domain_name: *const c_char) -> bool {
             let (sender, receiver) = std::sync::mpsc::sync_channel(1);
             channel.send(move |mut cx| {
                 let string = JsString::new(&mut cx, "require('./snorestop/index.js')");
+                let key = JsString::new(&mut cx, "__print");
+                let function = JsFunction::new(&mut cx, __print).expect("failed to create print function");
+                cx.global().set(&mut cx, key, function).expect("failed to set print global");
                 eval(&mut cx, string).unwrap();
                 sender.send(()).unwrap();
                 Ok(())
