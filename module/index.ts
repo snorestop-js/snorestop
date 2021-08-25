2e077c52bd9dccb2d6a5e48ed68073f7dafeea2a
Rose Hall <rose@hall.ly>
2021-08-25 17:18:30 -0400

    moved module

--- module/index.ts
+++
@@ -1,7 +0,0 @@
-export { SnorestopModule } from "./module";
-
-export class Snorestop {
-  load(packageJson: any, packageIndexPath: string): void {
-    new (require(packageIndexPath).default)();
-  }
-}
