80d33d57a339f85a2da71c0bb23ca05cc0cb9222
Sanae <atmatm6000@outlook.com>
2021-08-25 19:59:05 -0600

    Add il2cpp_alloc binding

--- snorestop/src/bindings.rs
+++ snorestop/src/bindings.rs
@@ -5,16 +5,17 @@ use nodejs::neon::context::{Context, FunctionContext};
 use nodejs::neon::handle::Handle;
 use nodejs::neon::object::Object;
 use nodejs::neon::result::JsResult;
-use nodejs::neon::types::{JsArray, JsFunction, JsNumber};
+use nodejs::neon::types::{JsArray, JsFunction, JsNumber, JsArrayBuffer};
 use paste::paste;
 use winapi::shared::minwindef::HMODULE;
 use winapi::um::libloaderapi::GetProcAddress;
 
 macro_rules! gen_statics {
-    {$($name: ident = ($($params: ty),*) -> $ret: ty),*} => {
+    ($($name: ident = ($($params: ty),*) -> $ret: ty),*) => {
         paste! {
             $(
-                #[allow(non_upper_case_globals)] static mut $name: Option<fn ($($params),*) -> $ret> = None;
+                #[allow(non_upper_case_globals)]
+                static mut $name: Option<fn ($($params),*) -> $ret> = None;
             )*
         }
     };
@@ -37,35 +38,50 @@ macro_rules! get_proc {
 }
 
 gen_statics! {
-    il2cpp_domain_get = () -> *mut usize,
-    il2cpp_domain_get_assemblies = (*mut usize, *mut usize) -> *mut *mut usize
+    il2cpp_domain_get = () -> *mut u8,
+    il2cpp_domain_get_assemblies = (*mut u8, *mut u8) -> *mut *mut u8,
+    il2cpp_alloc = (usize) -> *mut u8
 }
 
 fn domain_get(mut cx: FunctionContext) -> JsResult<JsNumber> {
-    Ok(cx.number(unsafe { (il2cpp_domain_get.unwrap()()) } as i32 as f64))
+    Ok(cx.number(unsafe { il2cpp_domain_get.unwrap()() } as i32 as f64))
 }
 
 fn domain_get_assemblies(mut cx: FunctionContext) -> JsResult<JsArray> {
     let domain: Handle<JsNumber> = cx.argument(0)?;
     let mut size = 0;
-    let assemblies = unsafe { il2cpp_domain_get_assemblies.unwrap()(domain.value(&mut cx) as i32 as *mut usize, &mut size) };
-    // println!("pizza {}", size);
+    let assemblies = unsafe { il2cpp_domain_get_assemblies.unwrap()(domain.value(&mut cx) as i32 as *mut u8, &mut size) };
     let array = cx.empty_array();
 
     unsafe {
         for i in 0..size {
-            set!(array, &mut cx, cx.number(i as f64), cx.number((*assemblies.add(i)) as i32 as f64));
+            set!(array, &mut cx, cx.number(i as f64), cx.number((*assemblies.add(i as usize)) as i32 as f64));
         }
     }
 
     Ok(array)
 }
 
+fn alloc(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
+    let size: Handle<JsNumber> = cx.argument(0)?;
+    let size = size.value(&mut cx);
+    if size > u32::MAX as f64 {
+        panic!("Cannot allocate >4GB buffers!");
+    }
+    let buffer = unsafe {
+        let memory = il2cpp_alloc.unwrap()(size as i32 as usize);
+        std::slice::from_raw_parts_mut(memory, size as usize)
+    };
+    Ok(JsArrayBuffer::external(&mut cx, buffer))
+}
+
 pub(crate) fn load_functions<'a, C: Context<'a>>(module: HMODULE, cx: &mut C) {
     let global_obj = cx.empty_object();
     get_proc!(module, il2cpp_domain_get);
     get_proc!(module, il2cpp_domain_get_assemblies);
+    get_proc!(module, il2cpp_alloc);
     set!(global_obj, cx, cx.string("il2cpp_domain_get"), JsFunction::new(cx, domain_get).expect("failed to create a js_function"));
     set!(global_obj, cx, cx.string("il2cpp_domain_get_assemblies"), JsFunction::new(cx, domain_get_assemblies).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_alloc"), JsFunction::new(cx, alloc).expect("failed to create a js_function"));
     set!(cx.global(), cx, "__IL2CPP", global_obj);
 }
