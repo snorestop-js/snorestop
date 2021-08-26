14357fc25a5da570588efdb900f43e7c138d06ff
Rose Hall <rose@hall.ly>
2021-08-25 22:01:02 -0400

    added more bindings

--- snorestop/src/bindings.rs
+++ snorestop/src/bindings.rs
@@ -1,11 +1,11 @@
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 use std::mem::transmute;
-
+use std::os::raw::c_char;
 use nodejs::neon::context::{Context, FunctionContext};
 use nodejs::neon::handle::Handle;
 use nodejs::neon::object::Object;
 use nodejs::neon::result::JsResult;
-use nodejs::neon::types::{JsArray, JsFunction, JsNumber, JsArrayBuffer};
+use nodejs::neon::types::{JsArray, JsFunction, JsNumber, JsString, JsUndefined, JsArrayBuffer};
 use paste::paste;
 use winapi::shared::minwindef::HMODULE;
 use winapi::um::libloaderapi::GetProcAddress;
@@ -38,6 +38,14 @@ macro_rules! get_proc {
 }
 
 gen_statics! {
+    il2cpp_assembly_get_image = (*mut usize) -> *mut usize,
+    il2cpp_image_get_name = (*mut usize) -> *mut c_char,
+    il2cpp_image_get_filename = (*mut usize) -> *mut c_char,
+    il2cpp_image_get_assembly = (*mut usize) -> *mut usize,
+    il2cpp_image_get_class_count = (*mut usize) -> usize,
+    il2cpp_image_get_class = (*mut usize, usize) -> *mut usize,
+    il2cpp_class_get_namespace = (*mut usize) -> *mut c_char,
+    il2cpp_class_get_name = (*mut usize) -> *mut c_char
     il2cpp_domain_get = () -> *mut u8,
     il2cpp_domain_get_assemblies = (*mut u8, *mut u8) -> *mut *mut u8,
     il2cpp_alloc = (usize) -> *mut u8
@@ -62,6 +70,71 @@ fn domain_get_assemblies(mut cx: FunctionContext) -> JsResult<JsArray> {
     Ok(array)
 }
 
+fn assembly_get_image(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let imagePtr = il2cpp_assembly_get_image.unwrap()(domain.value(&mut cx) as i32 as *mut usize);
+      cx.number(imagePtr as u32)
+    })
+}
+
+fn image_get_name(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let strPtr = il2cpp_image_get_name.unwrap()(domain.value(&mut cx) as i32 as *mut usize);
+      cx.string(CStr::from_ptr(strPtr).to_str().expect("Failed to unwrap strPtr"))
+    })
+}
+
+fn image_get_filename(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let strPtr = il2cpp_image_get_filename.unwrap()(domain.value(&mut cx) as i32 as *mut usize);
+      cx.string(CStr::from_ptr(strPtr).to_str().expect("Failed to unwrap strPtr"))
+    })
+}
+
+fn image_get_assembly(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let strPtr = il2cpp_image_get_assembly.unwrap()(domain.value(&mut cx) as i32 as *mut usize);
+      cx.number(strPtr as u32)
+    })
+}
+
+fn image_get_class_count(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let strPtr = il2cpp_image_get_class_count.unwrap()(domain.value(&mut cx) as i32 as *mut usize);
+      cx.number(strPtr as u32)
+    })
+}
+
+fn image_get_class(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let domain2: Handle<JsNumber> = cx.argument(1)?;
+      let strPtr = il2cpp_image_get_class.unwrap()(domain.value(&mut cx) as i32 as *mut usize, domain2.value(&mut cx) as usize);
+      cx.number(strPtr as u32)
+    })
+}
+
+fn class_get_namespace(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let strPtr = il2cpp_class_get_namespace.unwrap()(domain.value(&mut cx) as i32 as *mut usize);
+      cx.string(CStr::from_ptr(strPtr).to_str().expect("Failed to unwrap strPtr"))
+    })
+}
+
+fn class_get_name(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(unsafe {
+      let domain: Handle<JsNumber> = cx.argument(0)?;
+      let strPtr = il2cpp_class_get_name.unwrap()(domain.value(&mut cx) as i32 as *mut usize);
+      cx.string(CStr::from_ptr(strPtr).to_str().expect("Failed to unwrap strPtr"))
+    })
+}
+
 fn alloc(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
     let size: Handle<JsNumber> = cx.argument(0)?;
     let size = size.value(&mut cx);
@@ -79,7 +152,25 @@ pub(crate) fn load_functions<'a, C: Context<'a>>(module: HMODULE, cx: &mut C) {
     let global_obj = cx.empty_object();
     get_proc!(module, il2cpp_domain_get);
     get_proc!(module, il2cpp_domain_get_assemblies);
+    get_proc!(module, il2cpp_assembly_get_image);
+    get_proc!(module, il2cpp_image_get_name);
+    get_proc!(module, il2cpp_image_get_filename);
+    get_proc!(module, il2cpp_image_get_assembly);
+    get_proc!(module, il2cpp_image_get_class_count);
+    get_proc!(module, il2cpp_image_get_class);
+    get_proc!(module, il2cpp_class_get_namespace);
+    get_proc!(module, il2cpp_class_get_name);
     get_proc!(module, il2cpp_alloc);
+    set!(global_obj, cx, cx.string("il2cpp_domain_get"), JsFunction::new(cx, domain_get).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_domain_get_assemblies"), JsFunction::new(cx, domain_get_assemblies).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_assembly_get_image"), JsFunction::new(cx, assembly_get_image).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_image_get_name"), JsFunction::new(cx, image_get_name).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_image_get_filename"), JsFunction::new(cx, image_get_filename).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_image_get_assembly"), JsFunction::new(cx, image_get_assembly).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_image_get_class_count"), JsFunction::new(cx, image_get_class_count).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_image_get_class"), JsFunction::new(cx, image_get_class).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_class_get_namespace"), JsFunction::new(cx, class_get_namespace).expect("failed to create a js_function"));
+    set!(global_obj, cx, cx.string("il2cpp_class_get_name"), JsFunction::new(cx, class_get_name).expect("failed to create a js_function"));
     set!(global_obj, cx, cx.string("il2cpp_domain_get"), JsFunction::new(cx, domain_get).expect("failed to create a js_function"));
     set!(global_obj, cx, cx.string("il2cpp_domain_get_assemblies"), JsFunction::new(cx, domain_get_assemblies).expect("failed to create a js_function"));
     set!(global_obj, cx, cx.string("il2cpp_alloc"), JsFunction::new(cx, alloc).expect("failed to create a js_function"));
